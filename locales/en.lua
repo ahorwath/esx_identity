144d96b117d37d9fcf0000286293b80ff512bfdb
ahorwath <79194420+ahorwath@users.noreply.github.com>
2021-02-17 10:57:45 +0100

    Initial commit

---
+++ locales/en.lua
@@ -0,0 +1,11 @@
+Locales['en'] = {
+  ['show_registration'] = 'a Regisztráció menü megjelenítése',
+  ['show_active_character'] = 'Aktív karakter megjelenítése',
+  ['delete_character'] = 'töröld a karaktered, és hozz létre egy újat',
+  ['deleted_character'] = 'a karaktered törlésre került.',
+  ['not_registered'] = 'neked nincs karaktered regisztrálva.',
+  ['active_character'] = '~b~aktív karakter:~s~ %s %s',
+  ['already_registered'] = 'már regisztráltál egy karaktert.',
+  ['failed_identity'] = 'nem sikerült beállítani a karaktert, próbálkozzon újra később, vagy lépjen kapcsolatba a szerver adminisztrátorával!',
+  ['create_a_character'] = 'létre kell hozni egy karaktert a játékhoz.'
+}
